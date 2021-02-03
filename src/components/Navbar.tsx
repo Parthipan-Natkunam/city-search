46239768d67ce7245f9abadf828f60f8291bbc88
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-02-04 03:02:50 +0530

    add icon in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,17 @@ import * as React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
+import PageviewIcon from "@material-ui/icons/Pageview";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
       padding: theme.spacing(3),
+    },
+    icon: {
+      marginRight: theme.spacing(1),
+      verticalAlign: "bottom",
     },
   })
 );
@@ -21,7 +26,10 @@ const Navbar: React.FC<NavbarProps> = ({ brandTitle }): JSX.Element => {
 
   return (
     <AppBar position="static" className={classes.root}>
-      <Typography variant="h5">{brandTitle}</Typography>
+      <Typography variant="h5">
+        <PageviewIcon fontSize="large" className={classes.icon} />
+        {brandTitle}
+      </Typography>
     </AppBar>
   );
 };
